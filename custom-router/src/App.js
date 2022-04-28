942a76c405f7405c224e6c4086ba9d38a4a152fe
Ramiro Trovant <32982000+gustavazo@users.noreply.github.com>
2022-04-28 14:22:41 -0300

    Sabe

--- custom-router/src/App.js
+++ custom-router/src/App.js
@@ -3,10 +3,35 @@ import BrowserRouter from './components/browserRouter';
 import Routes from './components/routes';
 import Route from './components/route';
 
+import React from "react";
 
 function App() {
+  React.useEffect(() => {
+    // escucho al evento custom urlChanged y paso como argumento un callback que recibe el evento. Se va a ejecutar cada vez que se dispare el evento (document.dispatchEvent)
+    document.addEventListener("urlChanged", (e) => {
+      e.stopImmediatePropagation();
+      console.log(e); 
+    });
+  }, []);
+
+  const navigate = (newPath) => {
+    // crea un evento custom nuevo al que ya estamos subscritos en el useEffect
+    var pushChangeEvent = new CustomEvent("urlChanged", {
+      detail: {
+        path: newPath
+      }
+    });
+
+    // cambio la url
+    window.history.replaceState({}, '', newPath);
+    
+    // disparo el evento
+    document.dispatchEvent(pushChangeEvent);
+  }
+
   return (
     <BrowserRouter>
+      <button onClick={() => navigate("/dashboard")}>Navegar a dashboard</button>
       <Routes>
         <Route></Route>
       </Routes>
--- custom-router/src/components/browserRouter.js
+++ custom-router/src/components/browserRouter.js
@@ -1,6 +1,25 @@
+import React from "react";
 
-function BrowserRouter(params) {
 
-    return null
+function BrowserRouter({ children }) {
+    const [currentPath, setCurrentPath] = React.useState(window.location.pathname)
+
+    React.useEffect(() => {
+        // escucho al evento custom urlChanged y paso como argumento un callback que recibe el evento. Se va a ejecutar cada vez que se dispare el evento (document.dispatchEvent)
+        document.addEventListener("urlChanged", (e) => {
+            e.stopImmediatePropagation();
+            setCurrentPath(e.detail.path)
+        });
+
+        React.Children.forEach(children, (element) => {
+            console.log("ASD", element.name)
+        });
+    }, []);
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
 }
 export default BrowserRouter
